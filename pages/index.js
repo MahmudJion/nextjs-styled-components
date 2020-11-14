0c5c45745921367543529c5dafb2a82562eaa805
MahmudJion <43998247+MahmudJion@users.noreply.github.com>
2020-11-14 16:13:11 +0600

    init setup

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default () => (
+  <div>
+    <Title>Next.js Page with Styled Components</Title>
+  </div>
+);
+
+const Title = styled.h1`
+  color: red;
+`;
