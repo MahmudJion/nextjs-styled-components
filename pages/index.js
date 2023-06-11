04567eb9731221612a6c878bae7f10e629f55789
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-06-11 19:55:45 +0100

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
-export default () => (
-  <div>
-    <Title>Next.js Page with Styled Components</Title>
-  </div>
-);
-
 const Title = styled.h1`
   color: black;
 `;
+
+export default function MyComponent() {
+  return (
+    <div>
+      <Title>Next.js Page with Styled Components</Title>
+    </div>
+  );
+}
